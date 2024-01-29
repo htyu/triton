940ea117b01f9887b9509c819ccbbdba70fce246
Hongtao Yu <hoy@fb.com>
2024-01-29 13:53:43 -0800

    vecAdd rewriting

--- python/tutorials/01-vector-add.py
+++ python/tutorials/01-vector-add.py
@@ -22,15 +22,25 @@ import torch
 
 import triton
 import triton.language as tl
+from triton.runtime import driver
 
-
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_stages=2, num_warps=4),
+        triton.Config({}, num_stages=4, num_warps=4),
+        triton.Config({}, num_stages=8, num_warps=4),
+        triton.Config({}, num_stages=16, num_warps=4),
+        triton.Config({}, num_stages=32, num_warps=4),
+    ],
+    key=[],
+)
 @triton.jit
 def add_kernel(x_ptr,  # *Pointer* to first input vector.
                y_ptr,  # *Pointer* to second input vector.
                output_ptr,  # *Pointer* to output vector.
                n_elements,  # Size of the vector.
-               stride: tl.constexpr,  # Number of elements each program should process.
                BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+               stride: tl.constexpr,  # Number of elements each program should process.
                # NOTE: `constexpr` so it can be used as a shape value.
                ):
     # There are multiple 'programs' processing different data. We identify which program
@@ -67,12 +77,17 @@ def add(x: torch.Tensor, y: torch.Tensor):
     # The SPMD launch grid denotes the number of kernel instances that run in parallel.
     # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
     # In this case, we use a 1D grid where the size is the number of blocks:
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    num_sm = 132*2
+    #device = torch.cuda.current_device() * 2
+    #num_sm = driver.active.utils.get_device_properties(device)["multiprocessor_count"]
+    grid = lambda meta: (num_sm, )
+    BLOCK_SIZE = triton.next_power_of_2(n_elements // num_sm)
+    # print(BLOCK_SIZE)
     # NOTE:
     #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
     #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
     #  - Don't forget to pass meta-parameters as keywords arguments.
-    kernel_info = add_kernel[grid](x, y, output, n_elements, stride=256, BLOCK_SIZE=1024)
+    kernel_info = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=BLOCK_SIZE, stride=512)
     for ir in ["ttir", "ttgir", "llir", "ptx"]:
         with open(f"{ir}.txt", "w") as f:
             f.write(kernel_info.asm[ir])
@@ -111,7 +126,8 @@ print(f'The maximum difference between torch and triton is '
 @triton.testing.perf_report(
     triton.testing.Benchmark(
         x_names=['size'],  # Argument names to use as an x-axis for the plot.
-        x_vals=[2**i for i in range(12, 28, 1)],  # Different possible values for `x_name`.
+        #x_vals=[2**i for i in range(12, 28, 1)],  # Different possible values for `x_name`.
+        x_vals=[2**30],  # Different possible values for `x_name`.
         x_log=True,  # x axis is logarithmic.
         line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
         line_vals=['triton', 'torch'],  # Possible values for `line_arg`.

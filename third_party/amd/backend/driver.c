aaf0100208425d7d5f3ccd32bff34f82677acc01
Hongtao Yu <hoy@fb.com>
2024-05-03 10:47:54 -0700

    Expose more hardware properties.

--- third_party/amd/backend/driver.c
+++ third_party/amd/backend/driver.c
@@ -130,11 +130,13 @@ static PyObject *getDeviceProperties(PyObject *self, PyObject *args) {
 
   // create a struct to hold device properties
   return Py_BuildValue("{s:i, s:i, s:i, s:i, s:i, s:s, s:i}", "max_shared_mem",
-                       props.sharedMemPerBlock, "multiprocessor_count",
-                       props.multiProcessorCount, "sm_clock_rate",
-                       props.clockRate, "mem_clock_rate", props.memoryClockRate,
-                       "mem_bus_width", props.memoryBusWidth, "arch",
-                       props.gcnArchName, "warpSize", props.warpSize);
+                       props.sharedMemPerBlock, "max_num_regs",
+                       props.regsPerBlock, "multiprocessor_count",
+                       props.warpSize, "warp_size", props.multiProcessorCount,
+                       "sm_clock_rate", props.clockRate, "mem_clock_rate",
+                       props.memoryClockRate, "mem_bus_width",
+                       props.memoryBusWidth, "arch", props.gcnArchName,
+                       "warpSize", props.warpSize);
 }
 
 static PyObject *loadBinary(PyObject *self, PyObject *args) {
